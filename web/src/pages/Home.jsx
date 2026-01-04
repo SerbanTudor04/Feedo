d840da266d45da2b3af534fe0541e74632fa6171
Serban Tudor Gabriel <tudor.gabriel.serban@gmail.com>
2026-01-04 22:01:10 +0200

    feat: Refactor Dockerfiles for multi-stage builds; optimize production dependencies and improve image size

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -38,7 +38,16 @@ export default function Home() {
         <Container maxWidth="lg">
           <Toolbar sx={{ justifyContent: 'space-between', py: 1 }}>
             <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5, cursor: 'pointer'  }}>
-              <SchoolIcon sx={{ color: '#137fec', fontSize: 32 }} />
+              <svg 
+                width="32" 
+                height="32" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                strokeWidth={1.5} 
+                stroke="black" // <-- Stroke setat pe negru
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.499 5.221 69.78 69.78 0 00-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+              </svg>
               <Typography variant="h6" sx={{ fontWeight: 900, tracking: '-0.015em' }}>
                 Feedo
               </Typography>
